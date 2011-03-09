b0d3002ee4cd1c1d52bbcce26fd439b63147305c
Timo Savola <timo.savola@iki.fi>
2011-03-09 22:07:05 +0200

    example: print Exception type name

--- example/example.cpp
+++ example/example.cpp
@@ -1,5 +1,10 @@
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <typeinfo>
+
+#include <cxxabi.h>
 
 #include <boost/scoped_array.hpp>
 
@@ -29,6 +34,15 @@ static CodeObject load_example_code()
 	return CodeObject::Load(buf.get(), size);
 }
 
+template <typename T>
+static std::string type_name(const T &object)
+{
+	auto c_str = abi::__cxa_demangle(typeid (object).name(), 0, 0, 0);
+	std::string str = c_str;
+	std::free(c_str);
+	return str;
+}
+
 int main()
 {
 	Context context;
@@ -39,7 +53,7 @@ int main()
 		while (executor.execute())
 			;
 	} catch (const Exception &e) {
-		std::cerr << e.what() << "\n";
+		std::cerr << type_name(e) << ": " << e.what() << "\n";
 		return 1;
 	}
 
