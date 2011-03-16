c543abe0713e145327207e67f3a0220177f1de11
Timo Savola <timo.savola@iki.fi>
2011-03-17 00:22:39 +0200

    example: pass Executor snapshot between processes

--- example/example.cpp
+++ example/example.cpp
@@ -12,6 +12,7 @@
 
 #include <boost/crc.hpp>
 #include <boost/scoped_array.hpp>
+#include <boost/scoped_ptr.hpp>
 
 #include <concrete/context.hpp>
 #include <concrete/exception.hpp>
@@ -44,7 +45,7 @@ static CodeObject load_code()
 	return code;
 }
 
-static ContextSnapshot load_snapshot(int fd)
+static ContextSnapshot load_context(int fd)
 {
 	ContextSnapshot snapshot;
 	ssize_t length;
@@ -52,14 +53,14 @@ static ContextSnapshot load_snapshot(int fd)
 	length = snapshot.head_size();
 
 	if (read(fd, snapshot.head_ptr(), length) != length)
-		throw std::runtime_error("read snapshot head");
+		throw std::runtime_error("read context snapshot head");
 
 	length = snapshot.data_size();
 	auto data = std::malloc(length);
 
 	if (read(fd, data, length) != length) {
 		std::free(data);
-		throw std::runtime_error("read snapshot data");
+		throw std::runtime_error("read context snapshot data");
 	}
 
 	snapshot.init_data(data);
@@ -67,19 +68,40 @@ static ContextSnapshot load_snapshot(int fd)
 	return snapshot;
 }
 
-static void save_snapshot(const ContextSnapshot &snapshot, int fd)
+static void save_context(const ContextSnapshot &snapshot, int fd)
 {
 	ssize_t length;
 
 	length = snapshot.head_size();
 
 	if (write(fd, snapshot.head_ptr(), length) != length)
-		throw std::runtime_error("write snapshot head");
+		throw std::runtime_error("write context snapshot head");
 
 	length = snapshot.data_size();
 
 	if (write(fd, snapshot.data_ptr(), length) != length)
-		throw std::runtime_error("write snapshot data");
+		throw std::runtime_error("write context snapshot data");
+}
+
+static bool load_executor(ExecutorSnapshot &snapshot, int fd)
+{
+	ssize_t length = read(fd, snapshot.ptr(), snapshot.size());
+
+	if (length == 0)
+		return false;
+
+	if (length == ssize_t(snapshot.size()))
+		return true;
+
+	throw std::runtime_error("read executor snapshot");
+}
+
+static void save_executor(const ExecutorSnapshot &snapshot, int fd)
+{
+	ssize_t length = snapshot.size();
+
+	if (write(fd, snapshot.ptr(), length) != length)
+		throw std::runtime_error("write executor snapshot");
 }
 
 template <typename T>
@@ -89,25 +111,6 @@ static std::string type_name(const T &object)
 	std::string str = c_str;
 	std::free(c_str);
 	return str;
-}
-
-static void work(Context &context)
-{
-	ContextScope scope(context);
-
-	std::cerr << "EXAMPLE: " << getpid() << ": execution begin" << std::endl;
-
-	try {
-		Executor executor(load_code());
-
-		while (executor.execute())
-			context.arena().check_error();
-
-	} catch (const Exception &e) {
-		std::cerr << "EXAMPLE: " << type_name(e) << ": " << e.what() << "\n";
-	}
-
-	std::cerr << "EXAMPLE: " << getpid() << ": execution end" << std::endl;
 }
 
 static void dump(const ContextSnapshot &snapshot)
@@ -120,40 +123,94 @@ static void dump(const ContextSnapshot &snapshot)
 	std::cerr << "EXAMPLE: " << getpid() << ": arena checksum = " << crc32.checksum() << std::endl;
 }
 
-static void save(const ContextSnapshot &snapshot, int output_fd)
+static void dump(const ExecutorSnapshot &snapshot)
 {
-	dump(snapshot);
-
-	if (output_fd >= 0)
-		save_snapshot(snapshot, output_fd);
+	std::cerr << "EXAMPLE: " << getpid() << ": executor snapshot = "
+	          << int32_t(snapshot.initial_frame_id.offset()) << " "
+	          << int32_t(snapshot.current_frame_id.offset()) << std::endl;
 }
 
-static void initial_work(int input_fd, int output_fd)
+static void initial_work(int count, int input_fd, int output_fd)
 {
 	Context context;
 
-	save(context.snapshot(), output_fd);
+	dump(context.snapshot());
+	save_context(context.snapshot(), output_fd);
 }
 
-static void secondary_work(int input_fd, int output_fd)
+static void secondary_work(int count, int input_fd, int output_fd)
 {
-	Context context(load_snapshot(input_fd));
+	Context context(load_context(input_fd));
+	ContextScope scope(context);
 
 	dump(context.snapshot());
-	work(context);
-	save(context.snapshot(), output_fd);
+
+	try {
+		ExecutorSnapshot executor_snapshot;
+		boost::scoped_ptr<Executor> executor;
+
+		if (load_executor(executor_snapshot, input_fd)) {
+			std::cerr << "EXAMPLE: " << getpid() << ": executor snapshot found" << std::endl;
+
+			dump(executor_snapshot);
+
+			executor.reset(new Executor(executor_snapshot));
+		} else {
+			std::cerr << "EXAMPLE: " << getpid() << ": loading bytecode" << std::endl;
+
+			executor.reset(new Executor(load_code()));
+		}
+
+		std::cerr << "EXAMPLE: " << getpid() << ": execution begin" << std::endl;
+
+		for (int i = 0; count < 0 || i < count; i++) {
+			bool executing = executor->execute();
+			context.arena().check_error();
+
+			if (!executing)
+				break;
+		}
+
+		std::cerr << "EXAMPLE: " << getpid() << ": execution end" << std::endl;
+
+		dump(context.snapshot());
+		dump(executor->snapshot());
+
+		if (output_fd >= 0) {
+			save_context(context.snapshot(), output_fd);
+			save_executor(executor->snapshot(), output_fd);
+		}
+	} catch (const Exception &e) {
+		std::cerr << "EXAMPLE: " << getpid() << ": " << type_name(e) << ": " << e.what()
+		          << std::endl;
+	} catch (const std::exception &e) {
+		std::cerr << "EXAMPLE: " << getpid() << ": exception: " << e.what() << std::endl;
+	}
 }
 
-static pid_t start_worker(void func(int, int), int input_fd, int output_fd)
+static pid_t start_worker(void func(int, int, int), int count, int input_fd, int output_fd,
+                          int fd1, int fd2, int fd3)
 {
 	pid_t pid = fork();
 
 	if (pid == 0) {
-		func(input_fd, output_fd);
+		if (fd1 >= 0) close(fd1);
+		if (fd2 >= 0) close(fd2);
+		if (fd3 >= 0) close(fd3);
+
+		func(count, input_fd, output_fd);
 		std::exit(0);
 	}
 
 	return pid;
+}
+
+static void wait_worker(pid_t pid)
+{
+	int status = -1;
+	waitpid(pid, &status, 0);
+
+	std::cerr << "EXAMPLE: worker " << pid << " exited with status " << status << std::endl;
 }
 
 static int main()
@@ -166,15 +223,18 @@ static int main()
 
 	std::cerr << "EXAMPLE: starting workers" << std::endl;
 
-	pid_t p1 = start_worker(initial_work, -1, fd12[1]);
-	pid_t p2 = start_worker(secondary_work, fd12[0], fd23[1]);
-	pid_t p3 = start_worker(secondary_work, fd23[0], -1);
+	pid_t p1 = start_worker(initial_work,    0,      -1, fd12[1], fd12[0], fd23[0], fd23[1]);
+	pid_t p2 = start_worker(secondary_work, 60, fd12[0], fd23[1], fd12[1], fd23[0],      -1);
+	pid_t p3 = start_worker(secondary_work, -1, fd23[0],      -1, fd12[0], fd12[1], fd23[1]);
 
-	waitpid(p1, NULL, 0);
-	waitpid(p2, NULL, 0);
-	waitpid(p3, NULL, 0);
+	close(fd12[0]);
+	close(fd12[1]);
+	close(fd23[0]);
+	close(fd23[1]);
 
-	std::cerr << "EXAMPLE: workers exited" << std::endl;
+	wait_worker(p1);
+	wait_worker(p2);
+	wait_worker(p3);
 
 	return 0;
 }
