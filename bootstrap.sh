b85765ae1e47f8566a477eeceb30cc768c564a73
Timo Savola <timo.savola@iki.fi>
2011-06-12 19:58:27 +0300

    Boost 1.46.1 with checksum checking

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 git submodule update --init
-svn export http://svn.boost.org/svn/boost/tags/release/Boost_1_45_0/boost
+
+svn export http://svn.boost.org/svn/boost/tags/release/Boost_1_46_1/boost
+
+if ! find boost -not -type d | sort | xargs cat | md5sum | grep -q 98826cb1403b76f279147eefb333e614
+then
+	echo 2>&1 "Error: MD5 sum check of exported Boost headers failed"
+	exit 1
+fi
