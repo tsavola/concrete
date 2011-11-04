d02b1c93c9ad888efbc5b9e9d8aef7b88162a94f
Timo Savola <timo.savola@iki.fi>
2011-11-04 02:26:53 +0200

    autoconf

--- library/build.mk
+++ library/build.mk
@@ -1,9 +1,9 @@
 NAME		:= concrete
 VERSION		:= 0
-SOURCES		:= $(wildcard library/*.cpp library/*/*.cpp library/io/libanl/*.cpp)
+SOURCES		:= $(wildcard library/*.cpp library/*/*.cpp library/io/$(LIBRARY_RESOLVER)/*.cpp)
 CFLAGS		+= -fvisibility=hidden -fvisibility-inlines-hidden
 
 DEPENDS		+= $(O)/lib/libconcrete-core.pic.a
-LIBS		+= $(O)/lib/libconcrete-core.pic.a -levent -lanl
+LIBS		+= $(O)/lib/libconcrete-core.pic.a
 
 include build/library.mk
