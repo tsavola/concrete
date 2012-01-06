e05a3aea81117f5252608f5b122ba10eb5c041e3
Timo Savola <timo.savola@iki.fi>
2012-01-06 05:40:28 +0200

    copyright update

--- library/io/url.hpp
+++ library/io/url.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011  Timo Savola
+ * Copyright (c) 2011, 2012  Timo Savola
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
--- python/concrete.py
+++ python/concrete.py
@@ -1,4 +1,11 @@
-#!/usr/bin/env python3.1
+#
+# Copyright (c) 2011, 2012  Timo Savola
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
 
 __all__ = [
 	"Context",
