582cb756634525b39a66c4d7a0c8a5ffc0ec5858
Timo Savola <timo.savola@iki.fi>
2012-02-02 08:48:51 +0200

    retargetable TraceOutput()

---
+++ library/util/trace.cpp
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2012  Timo Savola
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#include <iostream>
+
+#include <concrete/util/trace.hpp>
+
+namespace concrete {
+
+void TraceOutput(const std::string &line)
+{
+	std::string s = line;
+	s += "\n";
+	std::cout << s;
+}
+
+} // namespace
